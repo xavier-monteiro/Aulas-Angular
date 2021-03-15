8752f5b02d326a9347ed2f2f480ce63e74c824c4
xavier-monteiro <xaviermonteiro05@gmail.com>
2021-03-15 12:06:51 +0000

    Angular NgModels
    Added NgModels

--- aulas/src/app/app.module.ts
+++ aulas/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { CalculadoraComponent } from './calculadora/calculadora.component';
 import { ReadMeGeneratorComponent } from './read-me-generator/read-me-generator.component';
 import { HomeComponent } from './home/home.component';
 import { ExperimentsComponent } from './experiments/experiments.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { ExperimentsComponent } from './experiments/experiments.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

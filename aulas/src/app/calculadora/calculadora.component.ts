82de240b128a9b554b41b8eba33d4778c8da0407
xavier-monteiro <xaviermonteiro05@gmail.com>
2021-03-15 12:47:45 +0000

    Updated Calculator
    Made a basic calculator

--- aulas/src/app/calculadora/calculadora.component.ts
+++ aulas/src/app/calculadora/calculadora.component.ts
@@ -12,4 +12,32 @@ export class CalculadoraComponent implements OnInit {
   ngOnInit(): void {
   }
 
+    number1 : number=0;
+    number2 : number=0;
+    result : number=0;
+    sinal : string="+";
+
+    soma(){
+
+      switch(this.sinal)
+      {
+        case "+":
+          this.result = Number(this.number1)+Number(this.number2);
+          break;
+        case "-":
+          this.result = Number(this.number1)-Number(this.number2);
+          break;
+        case "*":
+          this.result = Number(this.number1)*Number(this.number2);
+          break;
+        case "/":
+          this.result = Number(this.number1)/Number(this.number2);
+          break;
+
+
+          default:
+            this.result = Number(this.number1)+Number(this.number2);
+            break;
+      }
+    }
 }
